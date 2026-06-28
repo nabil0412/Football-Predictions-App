f0cea4f13bb15f42e4fdf2005e1764a60178b904
nabil0412 <abdelrahman.nabil04@gmail.com>
2026-06-28 01:30:20 +0100

    temp debug: show stage in prediction form

--- components/PredictionForm.tsx
+++ components/PredictionForm.tsx
@@ -234,6 +234,9 @@ export function PredictionForm({ match, existing, wildcardUsed = {}, underdogTea
           </div>
         )}
 
+        {/* DEBUG - remove after testing */}
+        <div style={{ fontSize: 10, color: "red" }}>stage: {match.stage} | ko: {String(isKnockout)}</div>
+
         {/* Knockout extra options */}
         {isKnockout && result && !ambiguous && (
           <div style={{ marginTop: 14, borderTop: "1px solid var(--wc-border)", paddingTop: 14 }}>
